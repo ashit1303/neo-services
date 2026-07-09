800c09cf36e639f6a30208ea04683845d1c0a619
ashit1303 <kmr.ashit1303@gmail.com>
2026-07-09 11:32:37 +0000

    updated env

--- fortlio-be/config/index.ts
+++ fortlio-be/config/index.ts
@@ -5,7 +5,7 @@ const ALLOWED_ENVS = ['dev', 'prod', 'local'] as const;
 const rawEnv = (process.env.BUN_ENV || 'local').toLowerCase();
 const env = ALLOWED_ENVS.includes(rawEnv as any) ? rawEnv : 'local';
 const AWS_SECRET_NAME = `${env}/testing`;
-
+console.info('BUN_ENV', process.env.BUN_ENV);
 export const config: Config = {
   port: Number(process.env.PORT || 4020),
   env,
